602f812d7959941c1a8a9369194f4f120a586a2a
eabearden97 <ebearden@macalester.edu>
2020-06-19 11:39:30 -0500

    Did most CSS styling, tweaked HTML, started JS

---
+++ Module-2-Assessment/assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Variables
+var numWins = 0;
+var guessesRemaining = 15;
+var currentWord = MLB;
+var lettersGuessed = [];
+
+// References
+var winsRef = document.querySelector('#numWins');
+var guessesRemainingRef = document.querySelector('')
